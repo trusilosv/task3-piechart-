791d8a2d94b76da34bb84d6eec708c6d4ba04737
Trusilo Sergey <TrusiloSV@mail.ru>
2021-03-12 18:06:55 +0300

    feat:add form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-
+import { useState } from 'react'
 import PieChart from './pieChart/pieChart'
 import Sectors from './sectors'
 import './app.css'
 function App() {
-  const sectors = new Sectors([35,12,24,19],200);
+  const onSubmit = (e) => {
+
+    e.preventDefault();
+    setSectors(new Sectors(e.target[0].value.split(',').map(iter => +iter), +e.target[1].value))
+  };
+  const [sectors, setSectors] = useState(new Sectors([1, 2, 3, 4], 200));
   return (
     <div className="App">
-       <main>
-         <h1>PieChart</h1>
-         <PieChart sectors={sectors.getSectors()} colorLine='black'/>
-       </main>
+      <main>
+        <h1>PieChart</h1>
+        <form
+          onSubmit={onSubmit}>
+          <input type='text' placeholder='Array value' ></input>
+          <input type='number' min='120' placeholder='Radius' ></input>
+          <button type='submit' >OK</button>
+        </form>
+        <PieChart sectors={sectors.getSectors()} colorLine='black' />
+      </main>
     </div>
   );
 }
